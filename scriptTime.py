72503145dd6013746b46f0a9fa2338bf2db0ef5b
Vandita <vanand@splunk.com>
2018-07-18 10:36:49 -0700

    Changed num of threads to 7, sends gen.json at around 2:45 sec

--- scriptTime.py
+++ scriptTime.py
@@ -3,6 +3,7 @@ import json
 import time
 import urllib3
 import config
+import sys
 from multiprocessing.dummy import Pool as ThreadPool
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -11,12 +12,13 @@ filename = config.filename
 url = config.url
 token = config.token
 authHeader = {'Authorization': 'Splunk %s' % (token)}
-numThreads = 8
+numThreads = 7
 counter = 0
 
 def sendEvent(event):
 
     global counter
+    
     try:
         epochTime = convertToEpoch(event['result']['_time'])
 
@@ -26,6 +28,7 @@ def sendEvent(event):
 
     except:
         print ("Error found, script stopped. Before error, we sent %d events to Splunk." % counter)
+        sys.exit()
 
     else:
         #call request to send event to HEC
@@ -33,9 +36,9 @@ def sendEvent(event):
 
         #if event is not successfully sent through HEC
         if r.text != '{"text":"Success","code":0}':
+            print(r.text)
             print ("Error found, script stopped. Before error, we sent %d events to Splunk." % counter)
-            
-    print(r.text)        
+            sys.exit()
     
     #keep track of how many events are being sent to Splunk
     counter +=1
@@ -54,16 +57,22 @@ def main():
     data = []
 
     #read json file and load in events into array
-    with open(filename) as json_file:
-        for line in json_file:
-            data.append(json.loads(line))
+    try: 
+        with open(filename) as json_file:
+            for line in json_file:
+                data.append(json.loads(line))
+    
+    except:
+        print ("Error found, script stopped. Before error, we sent %d events to Splunk." % counter)
+        sys.exit()
 
     #set number of threads
     pool = ThreadPool(numThreads)
 
     #python multithreading - pool.map(my_function, my_array) 
     results = pool.map(sendEvent, data)
-    print(counter)
+
+    print("Done! %d events sent to Splunk successfully." % counter)
     pool.close()
     pool.join()
 
